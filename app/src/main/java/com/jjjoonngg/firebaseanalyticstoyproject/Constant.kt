36c846443b19862db952b3f5abd53108c11e2bd3
JJJoonngg <whdtls3878@gmail.com>
2020-07-23 16:30:54 +0900

    사용 상수 적용

---
+++ app/src/main/java/com/jjjoonngg/firebaseanalyticstoyproject/Constant.kt
@@ -0,0 +1,10 @@
+package com.jjjoonngg.firebaseanalyticstoyproject
+
+class Constant {
+    companion object {
+        const val DEFAULT = 0
+        const val TYPE_LOGIN = 1
+        const val TYPE_SIGN_UP = 2
+        const val TYPE_TUTORIAL = 3
+    }
+}
